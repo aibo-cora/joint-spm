565f6ac840ac6c62c124c4a49047269cb9cb675c
Yura Filatov <yura.fila@gmail.com>
2022-04-30 14:32:24 -0400

    Local binary target. v1.0.4

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "joint-spm",
-            targets: ["joint-spm"]),
+            targets: ["joint-spm", "Joint"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,6 +21,9 @@ let package = Package(
         .target(
             name: "joint-spm",
             dependencies: []),
+        .binaryTarget(
+            name: "Joint",
+            path: "XCFramework/Joint.xcframework"),
         .testTarget(
             name: "joint-spmTests",
             dependencies: ["joint-spm"]),
